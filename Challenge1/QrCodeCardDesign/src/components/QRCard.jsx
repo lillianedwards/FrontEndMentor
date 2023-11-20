26b167c2d5b63a8a9dc4a6b1c0546334e799eb46
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-11-20 08:23:31 -0600

    REad me and deploy

--- Challenge1/QrCodeCardDesign/src/components/QRCard.jsx
+++ Challenge1/QrCodeCardDesign/src/components/QRCard.jsx
@@ -5,11 +5,11 @@ import qrImg from '../../qr-code-component-main/images/image-qr-code.png'
 function QRCard() {
   return (
     <div className="contain">
-      <Card className="card">
-        <Card.Img variant="top" src={qrImg} className="qr" />
+      <Card id="card">
+        <Card.Img variant="top" src={qrImg} id="qr" />
         <Card.Body>
-          <Card.Title className="title">Improve your front-end skills by building projects</Card.Title>
-          <Card.Text className="details">
+          <Card.Title id="title">Improve your front-end skills by building projects</Card.Title>
+          <Card.Text id="details">
             Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
           </Card.Text>
         </Card.Body>
@@ -20,7 +20,7 @@ function QRCard() {
           Frontend Mentor 
         </a>
         <br />
-        Coded by <a href="#">Lillian Edwards</a>.
+        Coded by <a href="https://www.linkedin.com/in/lillian-edwards63/" target="_blank">Lillian Edwards</a>
       </div>
     </div>
   );
